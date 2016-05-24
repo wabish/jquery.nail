59c6e51a7bd26809193abddb49d1f92ea8c8b4e9
cobish <cobish110@gmail.com>
2016-05-24 11:19:52 +0800

    add description

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # jquery.nail
+
+> A jQuery plugin that enables a label to be nailed in the loacation you want.
+
+## Demo
+
+暂时还没有。
+
+## Browser Support
+
+``chrome`` ``firefox``
+
+## How to use
+
+Simply include the `jquery.nail.min.js` file and place the following in the head of your document (make sure **jQuery** is included).
+
+### Minimum setup
+
+``` javascript
+$(function() {
+    $('#box').nail();
+});
+```
+
+#### Example with default options
+
+``` javascript
+$(function() {
+    $('#box').nail({
+        onTop: function(elem) {
+            elem.css('background', '#869EB5');
+        },
+        onBottom: function(elem) {
+            elem.css('background', 'rgb(34, 89, 58)');
+        },
+        onFixed: function(elem) {
+            elem.css('background', '#000');
+        }
+    });
+});
+```
+
+## License
+
+This plugin is available under [the MIT license](http://mths.be/mit).
--- dist/jquery.nail.js
+++ dist/jquery.nail.js
@@ -6,75 +6,107 @@
  * Copyright (c) 2016, cobish
  * Released under the MIT license.
  */
-(function($, window, document) {
-    $.fn.nail = function(options) {
-        var _this = this;
+(function($, window) {
 
-        var defaults = {
-            subElem: _this.parent(),
-            topCB: null,
-            fixedCB: null,
-            bottomCB: null
-        }
+    var methods = {
 
-        var options = $.extend(defaults, options || {});
+        options: null,
 
-        var oWindow = $(window),
-            oDocument = $(document);
+        defaults: null,
 
-        var subTop = options.subElem.offset().top,
-            subHeight = options.subElem.height(),
-            nailTop = _this.offset().top,
-            nailHeight = _this.height();
+        init: function(elem, options) {
+            this.options = $.extend({
+                elem: $(elem),
+                subElem: $(elem).parent(),
+                oWindow: $(window),
+                onTop: function(elem) {
+                    // 顶部触发，返回本身
+                },
+                onBottom: function(elem) {
+                    // 顶部触发，返回本身
+                },
+                onFixed: function(elem) {
+                    // 钉住触发，返回本身
+                }
+            }, options || {});
 
-        function handleScroll() {
-            var scrollTop = oWindow.scrollTop();
+            this.defaults = {
+                elemTop: 0,
+                elemHeight: this.options.elem.height(),
+                subElemTop: 0,
+                subElemLeft: 0,
+                subElemHeight: this.options.subElem.height()
+            };
+
+            var self = this;
+
+            this.options.oWindow.on('scroll', function() {
+                self._handleScroll();
+            });
+            this.options.oWindow.on('resize', function() {
+                self._handleResize();
+            });
+        },
+
+        _handleScroll: function() {
+            var scrollTop = this.options.oWindow.scrollTop(),
+                scrollLeft = this.options.oWindow.scrollLeft(),
+                elemHeight = this.defaults.elemHeight,
+                subElemTop = this.options.subElem.offset().top,
+                subElemLeft = this.options.subElem.offset().left,
+                subElemHeight = this.defaults.subElemHeight,
+                elem = this.options.elem,
+                subElem = this.options.subElem;
 
             // 滚到顶部
-            if (scrollTop <= subTop) {
-                _this.css({
+            if (scrollTop <= subElemTop) {
+                elem.css({
                     position: 'absolute',
-                    top: 0
+                    top: 0,
+                    left: parseFloat(elem.css('marginLeft')) + parseFloat(elem.css('paddingLeft')) + 'px'
                 });
 
-                if (options.topCB && typeof options.topCB === 'function') {
-                    options.topCB(_this);
+                if (this.options.onTop && typeof this.options.onTop === 'function') {
+                    this.options.onTop(elem);
                 }
 
                 return;
             }
 
             // 滚到底部
-            if (scrollTop >= subTop + subHeight - nailHeight) {
-                _this.css({
+            if (scrollTop >= subElemTop + subElemHeight - elemHeight) {
+                elem.css({
                     position: 'absolute',
-                    top: subHeight - nailHeight + 'px'
+                    top: subElemHeight - elemHeight + 'px',
+                    left: parseFloat(elem.css('marginLeft')) + parseFloat(elem.css('paddingLeft')) + 'px'
                 });
 
-                if (options.bottomCB && typeof options.bottomCB === 'function') {
-                    options.bottomCB(_this);
+                if (this.options.onBottom && typeof this.options.onBottom === 'function') {
+                    this.options.onBottom(elem);
                 }
 
                 return;
             }
 
-            _this.css({
+            elem.css({
                 position: 'fixed',
-                top: 0
+                top: 0,
+                left: subElemLeft + parseFloat(elem.css('marginLeft')) + parseFloat(elem.css('paddingLeft')) - scrollLeft + 'px'
             });
 
-            if (options.fixedCB && typeof options.fixedCB === 'function') {
-                options.fixedCB(_this);
+            if (this.options.onFixed && typeof this.options.onFixed === 'function') {
+                this.options.onFixed(elem);
             }
+        },
+
+        _handleResize: function() {
+            this._handleScroll();
         }
-
-        function handleResize() {
-
-        }
-
-        oWindow.on('scroll', handleScroll);
-        oWindow.on('resize', handleResize);
-
-        return _this;
     };
-})(jQuery, window, document);
+
+    $.fn.nail = function(options) {
+        methods.init(this, options);
+        return this;
+    };
+
+})(jQuery, window);
